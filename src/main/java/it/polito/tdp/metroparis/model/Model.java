1f9931a198ba2cb507426faa3c68a40be3426029
ADMIN <ADMIN@192.168.1.68>
2023-01-07 12:21:55 +0100

    nuovo

--- src/main/java/it/polito/tdp/metroparis/model/Model.java
+++ src/main/java/it/polito/tdp/metroparis/model/Model.java
@@ -1,5 +1,6 @@
 package it.polito.tdp.metroparis.model;
 
+import java.io.InvalidClassException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -43,8 +44,8 @@ public class Model {
 		List<Fermata> percorso = new ArrayList<>();
 	
 		while(corrente != null) {
-			percorso.add(0, corrente);
-			corrente = alberoInverso.get(corrente);
+			percorso.add(0, corrente); //inserisco in testa così che ogni volta che aggiungo 'corrente' nuovo si inserisce come primo elemento
+			corrente = alberoInverso.get(corrente);  //se avessi scritto: percorso.add(corrente) avrei avuto percorso che va dalla stazione di arrivo a quella di partenza
 		}
 		
 		return percorso ;
@@ -57,6 +58,64 @@ public class Model {
 		Graphs.addAllVertices(this.grafo, getFermate());
 		
 		MetroDAO dao = new MetroDAO();
+		
+//		METODO 1 -> itero su ogni coppia di vertici..aggiungo
+//		   gli archi facendo fare il lavoro al database, ma se i vertici 
+//		   sono tanti come in questo caso (619), farò 619*619 query al database 
+//		   e quindi il tempo di una risposta risulta elevato
+		
+//		for(Fermata partenza: fermate) {                     
+//			for(Fermata arrivo: fermate) {                     
+//				if(dao.isFermateConnesse(partenza, arrivo)) {   
+//					this.grafo.addEdge(partenza, arrivo);      
+//				}
+//			}
+//		}
+		
+//		METODO 2a -> dato ciascun vertice, trova i vertici ad esso
+//		   adiacenti
+//		   Il DAO mi restituisce una lista di interi (id numerici)
+		
+//		for(Fermata partenza: fermate) {
+//			List<Integer> idConnesse = dao.getIdFermateConnesse(partenza);
+//				for(Integer i: idConnesse) {
+//					Fermata arrivo = null;
+//					for(Fermata f: fermate) {
+//						if(f.getIdFermata()==i) {
+//							arrivo = f;
+//							break;
+//						}
+//					}
+//				this.grafo.addEdge(partenza, arrivo);
+//			}
+//		}
+//		
+//		METODO 2b -> dato ciascun vertice, trova i vertici ad esso
+//		   adiacenti
+//Il DAO mi restituisce una lista di oggetti di tipo fermata ---> query annidata(sotto-query) p una join tra tabelle
+		
+//		for(Fermata partenza: fermate) {
+//			List<Fermata> arrivi = dao.getFermateConnesse(partenza);
+//			for(Fermata arrivo: arrivi) {
+//				this.grafo.addEdge(partenza, arrivo);
+//			}
+//		}
+		
+//		METODO 2c -> dato ciascun vertice, trova i vertici ad esso
+//		   adiacenti
+//		   Il DAO mi restituisce una lista di id numerici, che converto in
+//         oggetti tramite una Map<Integer, Fermata> --> Identity Map
+		
+//		for(Fermata partenza: fermate) {
+//			List<Integer> idConnesse = dao.getIdFermateConnesse(partenza);
+//			for(int id: idConnesse) {
+//				Fermata arrivo = fermateIdMap.get(id); //ti do un id numerico e con
+//				this.grafo.addEdge(partenza, arrivo);  //la mappa vado a ricercare con la get l'oggetto di riferimento
+//			}
+//		}
+		
+//		METODO 3 -> faccio una sola query che mi restituisce una coppia di fermate
+//				da collegare..delego quasi tutto il lavoro al database
 
 		List<CoppiaId> fermateDaCollegare = dao.getAllFermateConnesse();
 		for (CoppiaId coppia : fermateDaCollegare) {
@@ -64,22 +123,22 @@ public class Model {
 		}
 
 //		System.out.println(this.grafo);
-//		System.out.println("Vertici = " + this.grafo.vertexSet().size());
-//		System.out.println("Archi   = " + this.grafo.edgeSet().size());
+		System.out.println("Vertici = " + this.grafo.vertexSet().size());
+		System.out.println("Archi   = " + this.grafo.edgeSet().size());
 	}
 
 	
 	
 	public Map<Fermata, Fermata> visitaGrafo(Fermata partenza) {
-		GraphIterator<Fermata, DefaultEdge> visita = new BreadthFirstIterator<>(this.grafo, partenza);
-		
+		GraphIterator<Fermata, DefaultEdge> visita = new BreadthFirstIterator<>(this.grafo, partenza); //visita in ampiezza
+//		GraphIterator<Fermata, DefaultEdge> visita1 = new DepthFirstIterator<>(this.grafo, partenza);  //visita in profondità
 		Map<Fermata,Fermata> alberoInverso = new HashMap<>() ;
 		alberoInverso.put(partenza, null) ;
 		
 		visita.addTraversalListener(new RegistraAlberoDiVisita(alberoInverso, this.grafo));
 		while (visita.hasNext()) {
 			Fermata f = visita.next();
-//			System.out.println(f);
+      		System.out.println(f);
 		}
 		
 		
